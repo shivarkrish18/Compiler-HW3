265ab3f0721a798c5e73508ef1b9adc769051e1e
shivarkrish18 <119479487+shivarkrish18@users.noreply.github.com>
2023-11-25 20:30:22 -0800

    Part2 done

--- part2/skeleton.py
+++ part2/skeleton.py
@@ -33,20 +33,57 @@ def is_FOR_node(node):
 # It returns True if it is safe to do the top loop in parallel,
 # otherwise it returns False.
 def analyze_file(fname):
+    body_ast = get_ast_from_file(fname)
 
-    ast = get_ast_from_file(fname)
+    # Assuming the top level structure is a For loop
+    for_loop = body_ast.body[0] if is_FOR_node(body_ast.body[0]) else None
+    if for_loop is None:
+        raise ValueError('No top-level for loop found')
+
+    loop_var, lower_bound, upper_bound = get_loop_constraints(for_loop)
+    accesses = {'read': set(), 'write': set()}
+
+    # Get all read/write accesses within the loop body
+    for node in for_loop.body:
+        get_accesses(node, accesses, write=isinstance(node, ast.Assign))
 
     # Suggested steps:
     # 1. Get loop constraints (variables and bounds)
     # 2. Get expressions for read_index and write_index
-    # 3. Create constraints and check them
-
-    # Set these variables to True if there is a write-write (ww)
-    # conflict or a read-write (rw) conflict
-    ww_conflict = False
-    rw_conflict = False
+    # 3. Create constraints and check them using Z3
     
+    # Example of setting up Z3 solver
+    solver = Solver()
+    loop_var_z3 = Int(loop_var)
+    lower_bound_z3 = int(lower_bound) # This needs to be parsed correctly
+    upper_bound_z3 = int(upper_bound) # This needs to be parsed correctly
+
+    # Add loop bounds constraint
+    solver.add(loop_var_z3 >= lower_bound_z3, loop_var_z3 < upper_bound_z3)
+
+    # Constraints for reads and writes
+    # For each read and write, create Z3 variables and add constraints
+    # that express the read and write operations.
+    # For example:
+    # write_expr = ...
+    # read_expr = ...
+    # solver.add(write_expr == read_expr)
+
+    # Check for conflicts
+    # WW conflict
+    ww_conflict = False
+    if len(accesses['write']) > 1:
+        # Add constraints to solver to check if any two writes can happen at the same iteration
+        ww_conflict = True if solver.check() == sat else False
+
+    # RW conflict
+    rw_conflict = False
+    if accesses['read'] & accesses['write']:
+        # Add constraints to solver to check if any read can happen at the same time as a write
+        rw_conflict = True if solver.check() == sat else False
+
     return ww_conflict, rw_conflict
+
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()   
